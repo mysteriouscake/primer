df9b68130db6f7d07226c179173d0954c1bffa82
Theodore Vu <114425635+mysteriouscake@users.noreply.github.com>
2023-08-28 09:00:53 -0500

    ux/makefile

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
 
+int main(){
+    int input = 0;
+    std::cout<<"Enter limit: ";
+    std::cin<<input;
+    int output = [input];
+    for(int i = 0; i<=input; i++){
+        
+    }
+    return 0;
+}
--- makefile
+++ makefile
@@ -1,1 +1,11 @@
+pythagora5: main.o
+	g++ -g -Wall -std=c++20 main.o -o pythagora5
 
+main.o: main.cpp
+	g++ -g -Wall -std=c++20 -c main.cpp
+
+clean:
+	rm *.o pythagora5
+
+run:
+	./pythagora5
