2dd86f345d15b05b8a7ef5c84fe697ebac4930ff
Theodore <theo12398@outlook.com>
2023-09-05 18:54:43 -0500

    gitignore

--- main.cpp
+++ main.cpp
@@ -2,16 +2,16 @@
 
 int main(){
     int input = 0;
-    std::cout>>"Enter limit: ";
-    std::cin<<input;
-    int output = [input];
+    std::cout<<"Enter limit: ";
+    std::cin>>input;
+    int output[input];
     for(int i = 0; i<=input; i++){
         for(int ii = 1; ii<input; ii++){
-            output[ii] = input%i
+            output[ii] = input%i;
         }
     }
     for(int i = 0; i<=input; i++){
-        std::cout>>output[i]>>'\n';
+        std::cout<<output[i]<<'\n';
     }
     return 0;
 }
