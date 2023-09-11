39480bbfed933bb99b3003dc1e826e0be7c6946f
Theodore Vu <114425635+mysteriouscake@users.noreply.github.com>
2023-09-11 10:30:34 -0500

    optimized for efficency

--- main.cpp
+++ main.cpp
@@ -9,13 +9,13 @@ int main(){
     //for(int i = 0; i<=input; i++){
         for(int ii = 0; ii<input; ii++){
             double*results;
-            results = new double [input];
+            results = new double [input/2];
             bool*check;
-            check = new bool [input];
-            for(int j = 0; j<=input; j++){
+            check = new bool [input/2];
+            for(int j = 0; j<=input/2; j++){
                 results[j] = input%j;
             }
-            for(int jj = 1; jj<=(input); jj++){
+            for(int jj = 1; jj<=(input/2); jj++){
                 if(results[jj] == 0){
                     check[jj] = false;
                     output[jj] = check[jj];
